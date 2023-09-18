55c67482ca0c1192febd2d7625fcb40270adcf0c
Ajay <ajayajuu73@gmail.com>
2023-09-18 20:48:50 +0530

    new commt

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <p>Hrllo</p>
+    <p>hello</p>
+
     </>
   )
 }
